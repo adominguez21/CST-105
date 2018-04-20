0557cde54bb4737e1e9926caefc1f61a8c10bfd2
toshibagrey\windows <alondradominguez68@gmail.com>
2018-04-19 20:46:02 -0700

    excercise 1

---
+++ Topic_1/me.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class me {
+	public static void main (String args[]) {
+	
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("My name is");
+		String name = input.nextLine();
+		
+        System.out.println("My favorite color is");
+        String color = input.next();
+        
+        System.out.println("If I won the lottery, I would");
+        String lottery = input.nextLine();
+        
+        
+        String end= input.next();
+        
+        
+	}
+
+}
